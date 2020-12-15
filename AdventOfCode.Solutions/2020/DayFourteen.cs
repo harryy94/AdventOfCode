d08a725c8be5ae012c50985698847a4b65a52069
Harrison Young <Harrison.Young@mab.org.uk>
2020-12-15 07:46:36 +0000

    Day 14 Part 2

--- AdventOfCode.Solutions/2020/DayFourteen.cs
+++ AdventOfCode.Solutions/2020/DayFourteen.cs
@@ -19,7 +19,11 @@ namespace AdventOfCode.Solutions._2020
                 @"mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
 mem[8] = 11
 mem[7] = 101
-mem[8] = 0"
+mem[8] = 0",
+                @"mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1"
             };
 
         private readonly Regex MemMatchRegex = new Regex(@"^mem\[([0-9]*)\] = ([0-9]*)$");
@@ -30,7 +34,75 @@ mem[8] = 0"
 
             PartOneAnswer = Part1(inputLines).ToString();
 
-            PartTwoAnswer = "N.A";
+            PartTwoAnswer = Part2(inputLines).ToString();
+        }
+
+        private long Part2(List<string> input)
+        {
+            var currentMask = "";
+
+            var memory = new Dictionary<long, long>();
+
+            foreach (var line in input)
+            {
+                if (string.IsNullOrEmpty(line))
+                    continue;
+
+                if (line.StartsWith("mask"))
+                {
+                    currentMask = line.Split(' ').Last();
+                    continue;
+                }
+
+                var regexMatch = MemMatchRegex.Match(line);
+
+                var base10Index = long.Parse(regexMatch.Groups[1].Value);
+                var value = long.Parse(regexMatch.Groups[2].Value);
+
+                var binaryIndex = Convert.ToString(base10Index, 2).PadLeft(currentMask.Length, '0');
+
+                var maskedStringBuilderList = new List<StringBuilder>();
+                maskedStringBuilderList.Add(new StringBuilder());
+                for (var i = 0; i < currentMask.Length; i++)
+                {
+                    if (currentMask[i] == 'X')
+                    {
+                        var count = maskedStringBuilderList.Count;
+                        for (var index = 0; index < count; index++)
+                        {
+                            var sb = maskedStringBuilderList[index];
+                            var newSb = new StringBuilder();
+                            newSb.Append(sb);
+                            maskedStringBuilderList.Add(newSb);
+                        }
+                    }
+
+                    var alternate = false;
+                    for (var j = 0; j < maskedStringBuilderList.Count; j++)
+                    {
+                        if (currentMask[i] == 'X')
+                        {
+                            maskedStringBuilderList[j].Append(maskedStringBuilderList.Count / 2 > j ? 1 : 0);
+                            alternate = !alternate;
+                        }
+                        else if (currentMask[i] == '1')
+                        {
+                            maskedStringBuilderList[j].Append(1);
+                        }
+                        else if (currentMask[i] == '0')
+                        {
+                            maskedStringBuilderList[j].Append(binaryIndex[i]);
+                        }
+                    }
+                }
+
+                foreach (var indexBuilder in maskedStringBuilderList)
+                {
+                    var index = Convert.ToInt64(indexBuilder.ToString(), 2);
+                    memory[index] = value;
+                }
+            }
+            return memory.Sum(x => x.Value);
         }
 
         private long Part1(List<string> input)
